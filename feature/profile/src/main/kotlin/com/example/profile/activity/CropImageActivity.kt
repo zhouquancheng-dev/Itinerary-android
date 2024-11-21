b99fe0e6084498f4fb7eebf86016761dc6a6d3de
userzhou <ouserzhou@qq.com>
2024-11-22 01:07:21 +0800

    Fix: CropImageActivity

--- feature/profile/src/main/kotlin/com/example/profile/activity/CropImageActivity.kt
+++ feature/profile/src/main/kotlin/com/example/profile/activity/CropImageActivity.kt
@@ -1,15 +1,15 @@
 package com.example.profile.activity
 
-import android.os.Build
 import android.os.Bundle
-import android.view.WindowInsetsController
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.FragmentActivity
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.common.util.ext.addSystemBarsColorUpdate
 import com.example.profile.state.DialogType
 import com.example.profile.ui.CropImage
 import com.example.profile.vm.UploadImageViewModel
+import com.example.ui.R
 import com.example.ui.theme.JetItineraryTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,19 +21,7 @@ class CropImageActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // 设置状态栏字体颜色为白色
-            window.insetsController?.setSystemBarsAppearance(
-                0, // 清除 LIGHT_STATUS_BARS 标志
-                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-            )
-        } else {
-            // Android 11 以下的设备，使用 setSystemUiVisibility
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = 0
-        }
-
+        addSystemBarsColorUpdate(R.color.black, false)
         setContent {
             JetItineraryTheme {
                 val uploadVm = hiltViewModel<UploadImageViewModel>()
